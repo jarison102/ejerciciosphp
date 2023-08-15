3d1a55c93d6137b242f2f7b82d27504e5e38f80a
Jarison <Jarimices@gmail.com>
2023-08-14 21:58:19 -0500

    Puro Numero

--- ejercicio11.php
+++ ejercicio11.php
@@ -1,5 +1,5 @@
 <?php
-// Función para calcular y mostrar la inversión año a año
+// Definición de la función para calcular y mostrar la inversión año a año
 function inversion() {
     // Verificamos si se enviaron los datos del formulario
     if(isset($_POST['Cantidad_a_invertir'], $_POST['Interés_anual'], $_POST['Número_de_años'])) {
@@ -10,10 +10,18 @@ function inversion() {
 
         // Calculamos y mostramos el capital obtenido cada año
         for($i = 1; $i <= $Años; $i++) {
-            $Capital = $Invertir * (1 + ($Interes / 100)) ** $i; // Fórmula de interés compuesto
+            // Fórmula de interés compuesto
+            // Cálculo del capital acumulado para el año actual
+            $Capital = $Invertir * (1 + ($Interes / 100)) ** $i; 
+            
+            // Mostramos el resultado formateado
             echo "Año $i: Capital obtenido = $". number_format($Capital, 2) . "<br>";
         }
+    }else {
+        echo "MEeeeee";
     }
 }
-inversion()
+
+// Llamamos a la función para calcular y mostrar la inversión
+inversion();
 ?>
